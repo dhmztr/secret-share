b63f5e8d26ccf3b5687f23179ef3c546687a9e82
Lukidere <koniecznyrad@gmail.com>
2026-03-09 21:53:01 +0100

    Serwowanie statycznej strony [demo]

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -3,14 +3,17 @@ use axum::{
     routing::get,
     Router,
 };
+const ADDR: &str = "0.0.0.0:8080";
 use tower_http::services::{ServeDir,ServeFile};
 #[tokio::main]
 async fn main() {
-    let frontend_service = ServeDir::new("../../frontend/build")
-        .not_found_service("../../frontend/build/index.html");
+    let frontend_service = ServeDir::new("../../../frontend/build/")
+        .fallback(ServeFile::new("../../../frontend/build/"));
 
     let app = Router::new().fallback_service(frontend_service);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    let listener = tokio::net::TcpListener::bind(ADDR).await.unwrap();
+    println!("Serving on {ADDR}");
     axum::serve(listener,app).await.unwrap();
 
 
