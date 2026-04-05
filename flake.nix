0690f36a741a8c258ac17518f8ce1ce7a5d21cf9
Lukidere <you@example.com>
2026-04-05 17:04:47 +0200

    Added Nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Rust + Leptos development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustc
+            cargo
+            rust-analyzer
+            cargo-leptos
+            trunk
+            gcc
+            cargo
+            wasm-bindgen-cli
+            pkg-config
+            openssl
+          ];
+
+          shellHook = ''
+            export RUST_BACKTRACE=1
+          '';
+        };
+      });
+}
