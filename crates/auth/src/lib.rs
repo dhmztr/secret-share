4ba67d77767fdace6ab16ad9876dae783e561bc3
Lukidere <you@example.com>
2026-04-11 11:44:57 +0200

    Finalized new authentication and authorization

--- crates/auth/src/lib.rs
+++ crates/auth/src/lib.rs
@@ -28,7 +28,6 @@ fn get_jwt_secret() -> String {
 pub struct Claim {
     pub userid: String,
     pub tier: UserTiers,
-    pub quota_used:i32,
     pub expires: DateTime<Utc>
 
 }
@@ -38,7 +37,6 @@ pub async fn create_token(conn:&PgPool,user:&str) -> Result<String,UsersErrors> 
     let claim = Claim {
         userid: user_data.email,
         tier: user_data.tier,
-        quota_used: user_data.quota_used,
         expires: Utc::now() + chrono::Duration::hours(24)
     };
     let token = jsonwebtoken::encode(&jsonwebtoken::Header::default(), &claim, &jsonwebtoken::EncodingKey::from_secret(get_jwt_secret().as_bytes())).unwrap();
@@ -48,22 +46,17 @@ pub async fn create_token(conn:&PgPool,user:&str) -> Result<String,UsersErrors> 
 
 }
 
-pub async fn verify_token(token_to_check:Claim) -> Result<bool,UsersErrors>{
-    let token_data = jsonwebtoken::decode::<Claim>(&token_to_check.userid, 
+pub async fn verify_token(token_to_check:String) -> Result<String,UsersErrors>{
+    let token_data = jsonwebtoken::decode::<Claim>(&token_to_check, 
         &jsonwebtoken::DecodingKey::from_secret(get_jwt_secret().as_bytes()),
         &jsonwebtoken::Validation::default()).unwrap();
     if token_data.claims.expires < Utc::now() {
-        return Ok(false);
+        return Err(UsersErrors::Expired);
     }
-    Ok(true)
-}
-pub async fn fetch_quota_from_token(token_to_check:String) -> Result<i32,UsersErrors> {
-    let token_data = jsonwebtoken::decode::<Claim>(&token_to_check, &jsonwebtoken::DecodingKey::from_secret(get_jwt_secret().as_bytes()),
-        &jsonwebtoken::Validation::default()).unwrap();
-    Ok(token_data.claims.quota_used)
+    Ok(token_data.claims.userid)
 }
 
-pub async fn verify_user(conn:&PgPool,user:&str,password:&str) -> Result<String,UsersErrors> {
+pub async fn login_user(conn:&PgPool,user:&str,password:&str) -> Result<String,UsersErrors> {
     let user_data = db::fetch_user(conn,user).await?;
     let parsed_hash = PasswordHash::new(&user_data.password_hash).unwrap();
     let argon2 = Argon2::default();
@@ -73,8 +66,12 @@ pub async fn verify_user(conn:&PgPool,user:&str,password:&str) -> Result<String,
     }
 
 }
-pub async fn register_user(conn:&PgPool,user:&str,pswdhash: &str) -> Result<Uuid,UsersErrors> {
-    create_user(conn,user,pswdhash).await
-   
-    
+pub async fn register_user(conn:&PgPool,email:&str,pswdhash: &str) -> Result<String,UsersErrors> {
+        match create_user(conn,email,pswdhash).await {
+            Ok(_) => match create_token(conn, email).await {
+                Ok(val) => Ok(val),
+                Err(_) => Err(UsersErrors::TokenCreationFailed)
+            },
+            Err(e) => Err(e) 
+        }
 }
